7b5580c4a9cf69c4279a560676d5137c1985353d
ArendE <arend_eric@hotmail.com>
2015-02-23 12:06:20 +0100

    Added random flag color

--- country.py
+++ country.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+from flag_color import FlagColor
+
 class Country:
     ''' Country class to hold various country names '''
     
     def __init__(self, name):
         self.setName(name)
+        self.setRandomColor()
 
     def setName(self, name):
         ''' Sets the name of the country '''
@@ -13,6 +16,14 @@ class Country:
     def getName(self):
         ''' Returns the name of the country '''
         return self.name
+
+    def setRandomColor(self):
+        ''' Sets a random color for the country '''
+        self.flagColor = FlagColor()
+
+    def getColor(self):
+        ''' Returns the color of the country '''
+        return self.flagColor
 
     def __str__(self):
         return 'Hello from ' + self.name
